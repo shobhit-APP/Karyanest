f8da3a5ff7f571395b10e5079474bb819dfb193f
Shobhit Srivastava <shobhitsrivastava2004@gmail.com>
2025-04-23 08:04:08 +0530

    Update karyanestApplication.java

--- karyanestApplication/src/main/java/com/backend/karyanestApplication/karyanestApplication.java
+++ karyanestApplication/src/main/java/com/backend/karyanestApplication/karyanestApplication.java
@@ -1,16 +1,17 @@
 package com.backend.karyanestApplication;
 
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@EnableJpaRepositories
-@ComponentScan(basePackages = {"com.example.rbac", "com.backend.karyanestApplication", "com.example.module_b","com.example.Authentication"})
-public class karyanestApplication {
+@ComponentScan(basePackages = {
+		"com.example.rbac",
+		"com.example.module_b",
+		"com.example.Authentication"
+})
+public class KaryanestApplication {
 	public static void main(String[] args) {
-		SpringApplication.run(karyanestApplication.class, args);
+		SpringApplication.run(KaryanestApplication.class, args);
 	}
 }
