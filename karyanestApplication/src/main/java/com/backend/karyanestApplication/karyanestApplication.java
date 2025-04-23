2557c90276caf0ea353a2d926cd9acd0646b7cf1
Shobhit Srivastava <shobhitsrivastava2004@gmail.com>
2025-04-23 08:10:15 +0530

    Update karyanestApplication.java

--- karyanestApplication/src/main/java/com/backend/karyanestApplication/karyanestApplication.java
+++ karyanestApplication/src/main/java/com/backend/karyanestApplication/karyanestApplication.java
@@ -10,7 +10,7 @@ import org.springframework.context.annotation.ComponentScan;
 		"com.example.module_b",
 		"com.example.Authentication"
 })
-public class KaryanestApplication {
+public class karyanestApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(KaryanestApplication.class, args);
 	}
