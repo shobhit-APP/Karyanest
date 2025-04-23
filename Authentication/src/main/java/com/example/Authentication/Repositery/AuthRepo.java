7345376b51076e0100f33ee1f47fe07c6d318bb0
Shobhit Srivastava <shobhitsrivastava2004@gmail.com>
2025-04-23 07:15:47 +0530

    Delete Authentication directory

--- Authentication/src/main/java/com/example/Authentication/Repositery/AuthRepo.java
+++
@@ -1,8 +0,0 @@
-//package com.example.Authentication.Repositery;
-//
-//import com.example.Authentication.DTO.UserDTO;
-//import org.springframework.data.jpa.repository.JpaRepository;
-//
-//public interface AuthRepo  extends JpaRepository<UserDTO,Long> {
-//
-//}
