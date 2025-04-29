7ed88c2c12e635bc6238849d0feedf2d3808ecba
Shobhit Srivastava <shobhitsrivastava2004@gmail.com>
2025-04-29 12:44:47 +0530

    chnge in appliction

--- karyanestApplication/src/main/java/com/backend/karyanestApplication/karyanestApplication.java
+++ karyanestApplication/src/main/java/com/backend/karyanestApplication/karyanestApplication.java
@@ -3,13 +3,19 @@ package com.backend.karyanestApplication;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {
+		"com.example.Authentication.Controller", // exact case-sensitive path
+		"com.example.Authentication",
+		"com.example.rbac",
+		"com.backend.karyanestApplication",
+		"com.example.module_b",
+		"com.example.notification"
+})
 @EnableJpaRepositories
-@ComponentScan(basePackages = {"com.example.rbac", "com.backend.karyanestApplication", "com.example.module_b","com.example.Authentication","com.example.notification"})
 public class karyanestApplication {
+
 	public static void main(String[] args) {
 		SpringApplication.run(karyanestApplication.class, args);
 	}
