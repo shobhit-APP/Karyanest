44b40faeae03f302f659450abda18412d55e7637
Shobhit Srivastava <shobhitsrivastava2004@gmail.com>
2025-04-30 12:05:46 +0530

    change in appliction NotificationApplication.java

--- notification/src/main/java/com/example/notification/NotificationApplication.java
+++ notification/src/main/java/com/example/notification/NotificationApplication.java
@@ -6,16 +6,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.kafka.annotation.EnableKafka;
 
-@SpringBootApplication(scanBasePackages ={"com.example.notification"})
-@ComponentScan(basePackages = {"com.example.notification"})
-@EnableJpaRepositories
-@EnableAutoConfiguration
-@Configuration
+@EnableKafka
+@SpringBootApplication(scanBasePackages = {"com.example.notification"})
 public class NotificationApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(NotificationApplication.class, args);
 	}
-
 }
+
