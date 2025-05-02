453d986ac1aff6df2c7212cfe5c6e7f3fb6e83b8
Shobhit Srivastava <shobhitsrivastava2004@gmail.com>
2025-05-02 20:41:20 +0530

    change in CrosConfig

--- Authentication/src/main/java/com/example/Authentication/Middleware/CorsConfig.java
+++ Authentication/src/main/java/com/example/Authentication/Middleware/CorsConfig.java
@@ -3,13 +3,14 @@ package com.example.Authentication.Middleware;
 import jakarta.servlet.*;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
 
-@Component
+@Configuration
 @Order(Ordered.HIGHEST_PRECEDENCE) // Ensures this filter runs first
 public class CorsConfig implements Filter {
 
@@ -21,10 +22,15 @@ public class CorsConfig implements Filter {
         HttpServletRequest request = (HttpServletRequest) req;
 
         // Allow requests from frontend (React on port 3000)
-        response.setHeader("Access-Control-Allow-Origin", "http://localhost:3000"); 
-        response.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS"); 
-        response.setHeader("Access-Control-Allow-Headers", "*"); 
-        response.setHeader("Access-Control-Allow-Credentials", "true"); 
+        String origin = request.getHeader("Origin");
+        if (origin != null && origin.startsWith("http://localhost:")) {
+            response.setHeader("Access-Control-Allow-Origin", origin);
+        } else if (origin != null && origin.equals("https://nestaro.in")) {
+            response.setHeader("Access-Control-Allow-Origin", origin);
+        }
+        response.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+        response.setHeader("Access-Control-Allow-Headers", "*");
+        response.setHeader("Access-Control-Allow-Credentials", "true");
 
         // If it's a preflight request, respond directly
         if ("OPTIONS".equalsIgnoreCase(request.getMethod())) {
